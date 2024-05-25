1646a091b07a0d27a739d9c58862e133e13495aa
Veronica <vgalbicka@gmail.com>
2024-05-25 15:18:43 -0300

    Initial commit

---
+++ src/main/java/med_voll/api/controller/MedicoController.java
@@ -0,0 +1,14 @@
+package med_voll.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/medicos")
+public class MedicoController {
+    @PostMapping
+    public void registrarMedico(){
+        System.out.println("El request llega correctamente");
+    }
+}
